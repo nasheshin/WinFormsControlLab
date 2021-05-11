6309a8761c693eaa93409751d4a5e0242de8ab1b
nasheshin <nasheshin01@gmail.com>
2021-05-11 19:54:39 +0500

    Добавьте файлы проекта.

---
+++ TestControlsApplication/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TestControlsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numberBox1 = new LabControls.NumberBox(this.components);
+            this.SuspendLayout();
+            // 
+            // numberBox1
+            // 
+            this.numberBox1.ForeColor = System.Drawing.Color.Red;
+            this.numberBox1.Location = new System.Drawing.Point(12, 12);
+            this.numberBox1.Name = "numberBox1";
+            this.numberBox1.Size = new System.Drawing.Size(100, 23);
+            this.numberBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numberBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LabControls.NumberBox numberBox1;
+    }
+}
+
---
+++ WinFormsControlLab/NumberBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LabControls
+{
+    public partial class NumberBox : TextBox
+    {
+        public NumberBox()
+        {
+            InitializeComponent();
+        }
+
+        public NumberBox(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            double x;
+            if (!double.TryParse(Text, out x))
+                ForeColor = Color.Red;
+            else
+                ForeColor = Color.Black;
+            base.OnTextChanged(e);
+        }
+    }
+}
